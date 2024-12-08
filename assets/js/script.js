5e760467ae759f37cf8426f2f47a5a05138d6c75
Victor Hugo Guirra <victor.hugo.guirra@gmail.com>
2024-12-08 13:49:50 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
 const form = document.getElementById('form');
-
 form.addEventListener('submit', handleSubmit);
 
 function handleSubmit (event){
@@ -22,41 +21,24 @@ function handleSubmit (event){
     const gainWeight = maintenance + 450
 
     const layout = `
-    <h2>Aqui está o resultado:</h2>
+        <h2>Aqui está o resultado:</h2>
 
-    <div class="result-content">
-
-    <ul>
-
-      <li>
-        Seu metabolismo basal é de <strong>${tmb} calorias</strong>.
-      </li>
-
-      <li>
-        Para manter o seu peso você precisa consumir em média <strong>${maintenance} calorias</strong>.
-      </li>
-
-      <li>
-        Para perder peso você precisa consumir em média <strong>${loseWeight} calorias</strong>.
-      </li>
-
-      <li>
-        Para ganhar peso você precisa consumir em média <strong>${gainWeight} calorias</strong>.
-      </li>
-
-    </ul>
-
-  </div>
+        <div class="result-content">
+            <ul>
+                <li>Seu metabolismo basal é de <strong>${tmb} calorias</strong>.</li>
+                <li>Para manter o seu peso você precisa consumir em média <strong>${maintenance} calorias</strong>.</li>
+                <li>Para perder peso você precisa consumir em média <strong>${loseWeight} calorias</strong>.</li>
+                <li>Para ganhar peso você precisa consumir em média <strong>${gainWeight} calorias</strong>.</li>
+            </ul>
+        </div>
     `
 
     const result = document.getElementById('result')
-
     result.innerHTML = layout;
 }
 
 function getSelectedValue(id){
     const select = document.getElementById(id);
-
     return select.options[select.selectedIndex].value;
 }
 
